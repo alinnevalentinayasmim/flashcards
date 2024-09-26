2dbb6b4721f2b4511da4fb6b4d8e380b82b30e65
alinnevalentinayasmim <161481905+alinnevalentinayasmim@users.noreply.github.com>
2024-09-26 12:24:55 -0300

    z

--- main.js
+++ main.js
@@ -8,3 +8,7 @@ function areaQuadrado(lado) {
 return lado**2;
 }
 console.log(areaQuadrado(numero))
+let media=function(numero1,numero2,numero3){
+    return numero1+numero2+numero3/3;
+}
+document.write(numero)
